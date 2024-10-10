c414e5de80741eb2cf58b5a2406e4dcfcce96f54
Ricardo Gore <rjhgore@gmail.com>
2024-10-10 15:12:02 -0700

    Create Coverage.sh

---
+++ Coverage.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+#SBATCH --job-name=Coverage
+#SBATCH -N 1
+#SBATCH --mem=100GB
+#SBATCH --ntasks-per-node=20
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=rgomez@uabc.edu.mx
+
+
+EXPORT=/LUSTRE/apps/bioinformatica/hisat2/
+export PATH=$PATH:$EXPORT
+
+EXPORT=/LUSTRE/apps/bioinformatica/RSEM/bin/samtools-1.3
+export PATH=$PATH:$EXPORT
+
+EXPORT=/LUSTRE/apps/bioinformatica/stringtie/
+export PATH=$PATH:$EXPORT
+
+EXPORT=/LUSTRE/bioinformatica_data/genomica_funcional/apps/gffread
+export PATH=$PATH:$EXPORT
+
+EXPORT=/LUSTRE/apps/bioinformatica/TransDecoder-v5.7.0/
+export PATH=$PATH:$EXPORT
+
+EXPORT=/LUSTRE/bioinformatica_data/genomica_funcional/rgomez/Software/subread-2.0.6-Linux-x86_64/bin
+export PATH=$PATH:$EXPORT
+
+# Vars
+
+REFERENCE=$1 # Assembly file
+
+REF_PREFIX=`basename ${REFERENCE%.f*}`
+
+CPU=$SLURM_NPROCS
+
+
+# 1) Create index if does not exist
+
+mkdir -p INDEX
+
+if [ ! -f "INDEX/${REF_PREFIX}.1.ht2" ]; then
+hisat2-build -p $CPU $REFERENCE INDEX/$REF_PREFIX
+
+else
+    echo "index for '$REFERENCE' already exists."
+fi
+
+# 2) Align reads back to the reference
+
+WD=`pwd`
+WD=S1_HISAT2_SAM_BAM_FILES_${REF_PREFIX}_DIR
+
+mkdir -p $WD
+
+for i in $(ls *_R1.fq.gz)
+do
+bs="${i%*_R1.fq.gz}"
+
+# Test if the alignment was previously done!
+
+if [ ! -f "$WD/${bs}.sorted.bam.chkpt" ]; then
+
+left_file=${bs}_R1.fq.gz
+right_file=${bs}_R2.fq.gz
+
+    hisat2  --phred33 --dta -p $CPU \
+        -x INDEX/$REF_PREFIX -1 $left_file -2 $right_file \
+        --rg-id=${bs} --rg SM:${bs} -S $WD/${bs}.sam \
+        --summary-file ${bs}.summary.txt --met-file ${bs}.met.txt
+
+    samtools sort -@ $CPU -o $WD/${bs}.sorted.bam $WD/${bs}.sam
+
+    touch $WD/${bs}.sorted.bam.chkpt
+
+else 
+    echo "'$WD/${bs}.sorted.bam' already exists."
+    echo "Continue with sorting gtf."
+fi
+
+WD2=`pwd`
+WD2=S2_STRINGTIE_DENOVO_MODE${REF_PREFIX}_DIR
+
+
+mkdir -p $WD2
+
+if [ ! -f "$WD2/${bs}.gtf.chkpt" ]; then
+
+    stringtie --rf -p $CPU -l $bs -o $WD2/${bs}.gtf $WD/${bs}.sorted.bam
+
+    touch $WD2/${bs}.gtf.chkpt
+
+else
+ echo "$WD2/${bs}.gtf.chkpt already exists"
+fi
+
+done
+
+# Merging
+
+echo "Continue with next sample."
+
+ls -d -1 $WD2/*.gtf > stringtie_gtf_list.txt
+
+if [ ! -f "transcripts.gtf" ]; then
+ stringtie --rf --merge -p $CPU -o transcripts.gtf stringtie_gtf_list.txt
+
+else
+    echo "Merging step already exists. Continue w/ Feature Count"
+fi
+
+# 2) Count 
+ls -d -1 $WD/*.sorted.bam > sorted_bam_list.txt
+
+featureCounts -T $CPU -a transcripts.gtf -o feature_counts.txt sorted_bam_list.txt
+
+exit
