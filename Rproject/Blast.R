5970f116416be57d51e4a058464388ade38a72cc
Ricardo Gore <rjhgore@gmail.com>
2024-10-21 15:13:17 -0700

    Update Blast.R

--- Rproject/Blast.R
+++ Rproject/Blast.R
@@ -3,6 +3,8 @@
 # Intersected and distinct
 # Bind Subjects to conopeptides main groups (search how)
 
+library(tidyverse)
+
 read_outfmt6 <- function(f) {
   
   # seqid = transcript_id
@@ -30,8 +32,8 @@ f <- list.files(dir, pattern = "outfmt6", full.names = T)
 df <- do.call(rbind, lapply(f, read_outfmt6))
 
 df %>% 
-  # filter(identity > 97) %>%
-  distinct(subject)
+  filter(identity > 90) %>%
+  distinct(subject, identity)
 
 df %>% ggplot(aes(x = identity, y = db)) + ggridges::geom_density_ridges()
 
@@ -39,5 +41,9 @@ df %>% distinct(subject, db) %>%
   group_by(subject) %>%
   summarise(
     across(db, .fns = list), n = n(), .groups = "drop_last") %>%
-  filter(n == 1) %>% distinct(subject)
+  mutate(db = ifelse(n == 2, "Both", db)) %>%
+  mutate(db = unlist(db)) %>%
+  count(db) %>%
+  ggplot(aes(x = n, y = db)) +
+  geom_col()
   
