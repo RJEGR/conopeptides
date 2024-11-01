b0e20446a037f18457aa20be77442c26670a0b18
Ricardo Gore <rjhgore@gmail.com>
2024-11-01 09:24:38 -0700

    Update Conodictor.sh

--- Conodictor.sh
+++ Conodictor.sh
@@ -7,7 +7,8 @@
 #SBATCH --ntasks-per-node=24
 #SBATCH -t 06-00:00:00
 
-# 
+# The program requires only the assembled transcriptome or raw reads file, 
+# in either DNA or amino acid format. ConoDictor 2 automatically recognizes the alphabet used.
 NPROCS=$SLURM_NPROCS
 
 
@@ -21,3 +22,13 @@ conodictor --out ${FASTA%.*}_dir --cpus $NPROCS $FASTA
 exit
 
 # conodictor --out outfolder --cpus $NPROCS --mlen 51 file.fa
+# Fasta file must contain only ATCG characters
+EXPORT=/LUSTRE/bioinformatica_data/genomica_funcional/rgomez/Software/
+export PATH=$PATH:$EXPORT
+
+grep -v "^>" $FASTA  | grep -c "NNN" # 
+
+cat $FASTA | seqkit grep -v -s -i -p NNN > ${FASTA%.*}_no_redundant_NNN.fasta
+
+
+cat $FASTA | seqkit grep -s -i -p NNN | seqkit fx2tab -l 
