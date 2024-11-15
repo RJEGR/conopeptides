221d2ed1c65d76c49c439eb2031e30c39a81ba89
Ricardo Gore <rjhgore@gmail.com>
2024-11-15 11:42:33 -0800

    Create Lace.sh

---
+++ Lace.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH -p cicese
+#SBATCH --job-name=Lace
+#SBATCH -N 1
+#SBATCH --mem=100GB
+#SBATCH --ntasks-per-node=24
+#SBATCH -t 06-00:00:00
+
+# 
+
+module load conda-2024-2
+source activate lace
+export PATH=/LUSTRE/apps/bioinformatica/Lace-1.14.1/Lace:$PATH
+
+FASTA=$1
+
+OUTDIR=LACE_${FASTA%.*}_DIR
+
+mkdir -p $OUTDIR
+
+Lace $FASTA clusters.txt -o $OUTDIR
+
+exit
