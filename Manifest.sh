d23f022b0215e2ebe4121e005f841302d24a391e
Ricardo Gore <rjhgore@gmail.com>
2024-11-22 17:24:11 -0800

    Update Manifest.sh

--- Manifest.sh
+++ Manifest.sh
@@ -3,9 +3,6 @@
 # Create the sample data which contains four cols
 # Factor 1	Factor 2	Forward	Reverse
 
-WD=/LUSTRE/bioinformatica_data/genomica_funcional/rgomez/californicus/01.Preprocess/length_required_15/FASTP_OUT_DIR
-cd $WD
-
 for file in $(ls *_R1.fq.gz | grep fq)
 do
 basename=${file##*/}
