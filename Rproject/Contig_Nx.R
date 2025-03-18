c4c4371204f55bf5d43672b939fd6d2da88dd71f
Ricardo Gore <rjhgore@gmail.com>
2025-03-18 14:59:52 -0700

    Update Contig_Nx.R

--- Rproject/Contig_Nx.R
+++ Rproject/Contig_Nx.R
@@ -21,6 +21,11 @@ require(ggplot2)
 
 # dir <- "/Users/cigom/Documents/GitHub/conopeptides/02.Assembly/"
 # dir <- "/Users/cigom/Documents/GitHub/conopeptides/07.Reference/"
+
+pepdir <- "/Users/cigom/Documents/GitHub/conopeptides/05.Prediction/ConoSorter_dir/SORTED_regex_pHMM_dir/"
+pepf <- list.files(pepdir, "pep$", full.names = T)
+
+
 dir <- "/Users/cigom/Documents/GitHub/conopeptides/Nx_Metrics_dir/"
 
 f <- list.files(dir, "fasta$", full.names = T)
@@ -28,11 +33,15 @@ f <- list.files(dir, "fasta$", full.names = T)
 # f <- f[grepl("^transcripts.fasta|blastx", basename(f))]
 
 
-contig_Nx <- function(f) {
+contig_Nx <- function(f, stringSet = "DNA") {
   
-  DNA <- Biostrings::readDNAStringSet(f)
+  if(stringSet != "DNA") {
+    stringSet <- Biostrings::readAAStringSet(f)
+  } else
   
-  contig_width <- sort(Biostrings::width(DNA), decreasing = T)
+  stringSet <- Biostrings::readDNAStringSet(f)
+  
+  contig_width <- sort(Biostrings::width(stringSet), decreasing = T)
   
   # contig_df <- data.frame(width = contig_width, Assembly = basename(f))
   
@@ -40,11 +49,11 @@ contig_Nx <- function(f) {
 }
 
 
-metrics_df <- function(f) {
+metrics_df <- function(f, stringSet = "DNA") {
   
   # width <- sort(seq(1,100), decreasing = TRUE)
   
-  width <- contig_Nx(f)
+  width <- contig_Nx(f, stringSet = stringSet)
   
   assembly_seqs <- length(width)
   
@@ -82,12 +91,16 @@ metrics_df <- function(f) {
 
 # sum(metrics_df(f[1])$n_seqs)
 
+pepdf <- lapply(pepf, metrics_df, stringSet = "AA")
+pepdf <- do.call(rbind, pepdf)
+pepdf <- pepdf %>% mutate(stringSet = "B) Orfs (Transcriptome)")
+
+
 df <- lapply(f, metrics_df)
 
 df <- do.call(rbind, df)
 
 df <- mutate(df, x = factor(x, levels = unique(df$x)))
-
 
 unique(df$Assembly)
 
@@ -114,6 +127,27 @@ recode_to <- structure(
 
 df <- mutate(df, Assembly = dplyr::recode_factor(Assembly, !!!recode_to))
 
+recode_to <- c(
+  "SuperDuper_Spades_regex_pHMM.pep",
+  "spades_regex_pHMM.pep",
+  "Merged_hisat_SuperDuper_regex_pHMM.pep",
+  "MMseqs_hisat_SuperDuper_regex_pHMM.pep",
+  "MMseqs_regex_pHMM.pep",
+  "SuperDuper_Trinity_regex_pHMM.pep",
+  "Trinity_regex_pHMM.pep")
+
+recode_to <- structure(
+  c(
+    "Spades-Lace (Hisat)", 
+    "Spades (S)", 
+    "Concat-Lace T-S (Hisat)",
+    "MMseqs-Lace T-S (Hisat)",
+    "MMseqs (T-S)",
+    "Trinity-Lace (Hisat)",
+    "Trinity (T)"), 
+  names = recode_to)
+
+pepdf <- mutate(pepdf, Assembly = dplyr::recode_factor(Assembly, !!!recode_to))
 
 # dir <- "/Users/cigom/Documents/GitHub/conopeptides/04.Merge/transdecoder_dir/"
 # # dir <- "/Users/cigom/Documents/GitHub/conopeptides/07.Reference/"
@@ -124,24 +158,28 @@ df <- mutate(df, Assembly = dplyr::recode_factor(Assembly, !!!recode_to))
 
 # df <- rbind(df, df2)
 
+
+df <- df %>% mutate(stringSet = "DNA (Transcriptome)") %>% rbind(pepdf)
+
 # rnsps <- mean(contig_Nx(f[[1]]))
 # trnt <- mean(contig_Nx(f[[2]]))
 
 p <- ggplot(df, aes(x = x, y = n, group = Assembly, color = Assembly)) +
   geom_vline(xintercept = "N50", linetype="dashed", alpha=0.5) +
   ggplot2::geom_path(linewidth = 1.5, lineend = "round") +
+  facet_wrap(~  stringSet, scales = "free_y") +
   geom_point(shape = 21, size = 4, aes(size = n_frac)) +
   labs(x = "Nx", y = "Contig length", color = "Assembly method") +
   # ggsci::scale_color_jco() +
   ggsci::scale_color_startrek() +
-  guides(color=guide_legend(title = "", nrow = 1)) +
+  # guides(color=guide_legend(title = "", nrow = 2, ncol = 4)) +
   theme_bw(base_size = 14, base_family = "GillSans") +
   theme(legend.position = "top", 
     strip.background = element_rect(fill = 'grey89', color = 'white'),
     axis.line.x = element_blank(),
     axis.line.y = element_blank()) 
 
-p <- p + guides(color = guide_legend(title = "", nrow = 3, ncol = 3))
+p <- p + guides(color = guide_legend(title = "", nrow = 2, ncol = 4))
 
 p  
 
@@ -149,7 +187,7 @@ p
 
 # p
 
-ggsave(p, filename = 'Nx-methods.png', path = dir, width = 7.5, height = 5, device = png, dpi = 300)
+ggsave(p, filename = 'Nx-methods.png', path = dir, width = 10, height = 5, device = png, dpi = 300)
 
 p2 <- ggplot(df, aes(x = x, y = n_frac, group = Assembly, fill = Assembly)) +
   # ggplot2::geom_col() +
@@ -159,6 +197,23 @@ p2 <- ggplot(df, aes(x = x, y = n_frac, group = Assembly, fill = Assembly)) +
   # scale_fill_grey("") +
   ggsci::scale_fill_startrek() +
   guides(fill=guide_legend(title = "", ncol = 1)) +
+  facet_wrap(~  stringSet, scales = "free_y") + 
+  scale_x_discrete(position = "top") +
+  theme_bw(base_size = 14, base_family = "GillSans") +
+  theme(legend.position = "none", 
+    strip.background = element_rect(fill = 'grey89', color = 'white'),
+    axis.line.x = element_blank(),
+    axis.line.y = element_blank()) 
+
+df %>% 
+  group_by(stringSet, x) %>% mutate(frac = n/sum(n)) %>%
+  ggplot(aes(x = x, y = n_frac, group = Assembly, fill = Assembly)) +
+  facet_grid(Assembly ~  stringSet, scales = "free_y") + 
+  ggplot2::geom_col() +
+  labs(x = "", y = "Frac. of Scaffolds", fill = "Assembly method") +
+  # scale_color_grey("") +
+  # scale_fill_grey("") +
+  ggsci::scale_fill_startrek() +
   scale_x_discrete(position = "top") +
   theme_bw(base_size = 14, base_family = "GillSans") +
   theme(legend.position = "none", 
