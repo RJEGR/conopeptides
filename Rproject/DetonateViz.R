dfce85caa8f81bf709a2cbb48170ee0e0055be26
Ricardo Gore <rjhgore@gmail.com>
2025-06-12 16:04:23 -0700

    Update DetonateViz.R

--- Rproject/DetonateViz.R
+++ Rproject/DetonateViz.R
@@ -1,10 +1,25 @@
+
+# Purpose: Determine wheter the conflict in conotoxin nomenclature are true new putative conotoxins or artifacts
+
 # DetonateViz.R
 # Doint this for the 12,136 CDS
+# For transrate :
 # Read contigs.csv
 # Read bam_info.csv
 # select columns score (further read paper to understand values https://pmc.ncbi.nlm.nih.gov/articles/PMC4971766/)
 # parse to DataBase.R
 
+# For detonate, 
+# Read score.isoforms.results
+# select column score effective_length, contig_impact_score and IsoPct (omit because isoforms are unmasked)
+# IsoPct = (Transcript abundance / Total gene abundance) × 100
+# contig_impact_score = Relative contribution of a contig to explaining RNA-Seq data (Log ratio of true contig vs. noise hypothesis; used for trimming)
+
+# Technically, for each contig, the contig impact score is the log ratio of the probability that the contig represents a true biological transcript versus the probability that the contig is just background noise.
+
+# A higher contig impact score means the contig is more likely to be a true and important part of the transcriptome; a negative score suggests the contig is likely noise or redundant and could be trimmed from the assembly to improve overall accuracy.
+
+# This score enables assembly refinement by identifying and removing contigs that do not meaningfully explain the data, thereby improving assembly quality.
 
 #  AS INDIVIDUAL VALUES DOES NOT SUPPORT ANY COMPARISON, LETS COMPARE FROM THE TRUE-SET ANALYSIS 
 
@@ -18,7 +33,7 @@ options(stringsAsFactors = FALSE, readr.show_col_types = FALSE)
 
 pub_dir <- "/Users/cigom/Documents/GitHub/conopeptides/PUBLICATION_DIR/"
 
-dir <- "/Users/cigom/Documents/GitHub/conopeptides/03.Coverage/Contiguity_dir/conopeptides_detonate_dir/"
+dir <- "/Users/cigom/Documents/GitHub/conopeptides/03.Coverage/Contiguity_dir/Detonate_conopeptides_dir/"
 
 # LOAD data -----
 
@@ -35,14 +50,13 @@ f <- list.files(dir, "conopeptides.score.isoforms.results", full.names = T)
 
 scoresdf <- read_tsv(f)
 
+scoresdf <- scoresdf %>%  mutate(protein_id = sapply(strsplit(gene_id, "[|]"), `[`, 1)) %>% select(-transcript_id, -gene_id)
 
-scoresdf <- scoresdf %>% left_join(DB) %>% drop_na(tab) %>% filter(Signalp_class == "SP")
+scoresdf <- scoresdf %>% left_join(DB) # %>% 
+
+# IsoPct is commonly used as a threshold to remove lowly expressed or potentially misassembled isoforms from the assembly. For example, isoforms with an IsoPct of zero (i.e., not supported by any reads or fragments) are often removed, as they are likely artifacts
 
 scoresdf %>% count(IsoPct)
-
-# contig_impact_score
-
-# Critical metric: Higher values indicate transcripts likely to be artifacts (prioritize low-impact isoforms).
 
 # Filter artifacts: Remove isoforms with impact_score > 0.5 and TPM < 1.
 
@@ -50,9 +64,11 @@ scoresdf %>% select(contig_impact_score) %>% arrange(contig_impact_score)
 
 scoresdf %>% 
   mutate(Annotation = ifelse(is.na(Conflict), "A) No redundant", "B) Redundant (conflict)")) %>%
-  ggplot(aes(effective_length/length, log10(contig_impact_score))) + geom_point(aes(color = Annotation)) +
+  mutate(x = contig_impact_score, x = sign(x) * log(1+abs(x)) ) %>%
+  ggplot(aes(effective_length/length, x)) + geom_point(aes(color = Annotation)) +
   ggthemes::scale_color_calc() +
-  # labs(y = "", x = "Frac. of Effective length (Detonate)") +
+  facet_grid(~ Signalp_class) +
+  labs(y = "Contig impact score (pseudo-log transform)", x = "Frac. of Effective length (Detonate)") +
   theme_bw(base_family = "GillSans", base_size = 14) + theme(legend.position = "top")
 
 
@@ -63,29 +79,44 @@ scoresdf %>%
 
 scoresdf %>% ggplot(aes(contig_impact_score)) + geom_density()
 
+# Focus only in the conflicts for potential NEW
+
 scoresdf %>% 
+  mutate(Conflict = ifelse(is.na(Conflict), "A) No redundant", "B) Redundant (conflict)")) %>%
+  dplyr::count(tab, Conflict, Signalp_class)
+
+caption <- "A higher contig impact score means the contig is more likely to be a true and important part of the transcriptome\nA negative score suggests the contig is likely noise or redundant and could be trimmed from the assembly."
+
+scoresdf %>% 
+  drop_na(Superfamily, Conflict) %>%
+  filter(Signalp_class == "SP" & Score_sf != 1) %>%
   mutate(facet = ifelse(is.na(Conflict), "A) No redundant", "B) Redundant (conflict)")) %>%
-  ggplot(aes(y = Superfamily, x = contig_impact_score, fill = after_stat(x))) +
+  mutate(tab = ifelse(tab == "pHMM", "Novel candidates (pHMM)", "Homology predicted")) %>%
+  mutate(x = contig_impact_score, x = sign(x) * log(1+abs(x)) ) %>%
+  ggplot(aes(y = Superfamily, x = x, fill = after_stat(x))) +
   # geom_violin() +
-  facet_grid(~ facet) +
+  facet_grid(~ tab) +
   ggridges::geom_density_ridges_gradient(
-    jittered_points = T,
-    position = ggridges::position_points_jitter(width = 0.05, height = 0),
-    point_shape = '|', point_size = 3, point_alpha = 1, alpha = 1) +
+    jittered_points = F,
+    position = ggridges::position_points_jitter(width = 0.05, height = 0)) +
+  geom_point(shape = 124, size = 3) +
+  geom_vline(xintercept = 0, linetype="dashed", alpha=0.5) +
   scale_fill_viridis_c(option = "C") +
-  labs(y = "", x = "Effective length (Detonate)") +
-  theme_bw(base_family = "GillSans", base_size = 14) + theme(legend.position = "none")
+  labs(y = "Putative conotoxin with conflict assignment", x = "Contig impact score (pseudo-log transform)", caption = caption) +
+  theme_bw(base_family = "GillSans", base_size = 14) + theme(legend.position = "top")
 
-#
+# Focus only in the conflicts
 
 scoresdf %>% 
+  drop_na(Region) %>%
+  mutate(x = contig_impact_score, x = sign(x) * log(1+abs(x)) ) %>%
   mutate(facet = ifelse(is.na(Conflict), "A) No redundant", "B) Redundant (conflict)")) %>%
-  ggplot(aes(y = effective_length/length, x = facet, fill = after_stat(x))) +
+  ggplot(aes(y = x, x = facet, fill = after_stat(x))) +
   geom_violin() +
   geom_jitter(alpha = 0.5) +
-  # facet_grid(~ facet) +
+  facet_grid( ~ Region) +
   scale_fill_viridis_c(option = "C") +
-  labs(y = "", x = "Frac. of Effective length (Detonate)") +
+  labs(y = "Frac. of Effective length (Detonate)", x = "") +
   theme_bw(base_family = "GillSans", base_size = 14) + theme(legend.position = "none")
 
 
